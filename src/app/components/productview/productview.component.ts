fd2ad6ae79ec1cd063cf5c8ece0a14e919e2537f
nikhil Kumar <mail2nikhilkumar3@gmail.com>
2021-04-15 08:45:07 +0530

    adding productselect working

--- src/app/components/productview/productview.component.ts
+++ src/app/components/productview/productview.component.ts
@@ -9,7 +9,12 @@ declare var $: any;
 })
 export class ProductviewComponent implements OnInit,AfterContentInit {
 
- 
+ product1 : boolean = true;
+ product2 : boolean = false;
+ product3 : boolean = false;
+ product4 : boolean = false;
+
+
   constructor() { }
   ngAfterContentInit(): void {
    
@@ -21,9 +26,31 @@ export class ProductviewComponent implements OnInit,AfterContentInit {
 
      
 
-   
+  callproduct1(){
+    this.product1=true;
+    this.product2=false;
+    this.product3=false;
+    this.product4=false;
+  }
      
-
+  callproduct2(){
+    this.product1=false;
+    this.product2=true;
+    this.product3=false;
+    this.product4=false;
+  }
+  callproduct3(){
+    this.product1=false;
+    this.product2=false;
+    this.product3=true;
+    this.product4=false;
+  }
+  callproduct4(){
+    this.product1=false;
+    this.product2=false;
+    this.product3=false;
+    this.product4=true;
+  }
 
 
 }
